6958a84d466ebde4d3caaa767cd7ca7e99ae8170
Rondinele Guimaraes <rondineleg@edu.univai.br>
2019-05-06 09:34:08 -0300

    Criando classe para semear primeiro dados

---
+++ RazorBakery/Data/ModelBuilderDados.cs
@@ -0,0 +1,62 @@
+﻿using RazorBakery.Models;
+using Microsoft.EntityFrameworkCore;
+namespace RazorBakery.Data
+{
+    public static class ModelBuilderDados
+    {
+        public static ModelBuilder Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Produto>().HasData(
+                new Produto
+                {
+                    Id = 1,
+                    Nome = "Bolo de Cenoura",
+                    Descricao = "Uma Breve descricao do produto",
+                    Preco = 8.99m,
+                    NomeImagen = "boloCenoura.jpg"
+                },
+                new Produto
+                {
+                    Id = 2,
+                    Nome = "Torta de Limão",
+                    Descricao = "Uma Breve descricao do produto",
+                    Preco = 9.99m,
+                    NomeImagen = "tortaLimao.jpg"
+                },
+                new Produto
+                {
+                    Id = 3,
+                    Nome = "Cupcakes",
+                    Descricao = "Uma Breve descricao do produto",
+                    Preco = 5.99m,
+                    NomeImagen = "cupcakes.jpg"
+                },
+                new Produto
+                {
+                    Id = 4,
+                    Nome = "Pao",
+                    Descricao = "Uma Breve descricao do produto",
+                    Preco = 1.49m,
+                    NomeImagen = "pao.jpg"
+                },
+                new Produto
+                {
+                    Id = 5,
+                    Nome = "Torta de Pera",
+                    Descricao = " Uma Breve descricao do produto",
+                    Preco = 5.99m,
+                    NomeImagen = "tortaPera.jpg"
+                },
+                new Produto
+                {
+                    Id = 6,
+                    Nome = "Bolo de Chocolate ",
+                    Descricao = "Uma Breve descricao do produto",
+                    Preco = 8.99m,
+                    NomeImagen = "boloChocolate.jpg"
+                }
+            );
+            return modelBuilder;
+        }
+    }
+}
