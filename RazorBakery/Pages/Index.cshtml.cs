c121cf79843a013d47f06307cc16fa53f67697fb
Rondinele Guimaraes <rondineleg@edu.univai.br>
2019-05-06 11:31:30 -0300

    Atualizando index

--- RazorBakery/Pages/Index.cshtml.cs
+++ RazorBakery/Pages/Index.cshtml.cs
@@ -1,17 +1,29 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorBakery.Data;
+using RazorBakery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorBakery.Pages
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly RazorBakeryContext db;  
+        public IndexModel(RazorBakeryContext db) => this.db = db;
+        public List<Produto> Produtos { get; set; } = new List<Produto>();  
+        public IndexModel(Produto produtoDestaque) 
         {
-
+            this.ProdutoDestaque = produtoDestaque;
+               
+        }
+                public Produto ProdutoDestaque { get; set; }  
+        public async Task OnGetAsync()
+        {
+            Produtos = await db.Produtos.ToListAsync();
+            ProdutoDestaque = Produtos.ElementAt(new Random().Next(Produtos.Count));
         }
     }
 }
