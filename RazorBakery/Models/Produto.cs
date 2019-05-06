3298be30d2ae1fa0ab686ac5d6e7eedb5355a708
Rondinele Guimaraes <rondineleg@edu.univai.br>
2019-05-06 08:58:05 -0300

    Add class produto

---
+++ RazorBakery/Models/Produto.cs
@@ -0,0 +1,12 @@
+namespace RazorBakery.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+        public string NomeImagen { get; set; }        
+    
+    }
+}
